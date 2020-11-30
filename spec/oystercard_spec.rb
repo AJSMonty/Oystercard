05d9e4a63dc34b71d70f80d3ef523b9e72f6001a
AJ <aj.monty@gmail.com>
2020-11-30 16:08:28 +0000

    added top_up tests and code to pass tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,5 +5,9 @@ describe Oystercard do
   it { expect(subject).to be_kind_of(Oystercard) }
 
   it { expect(subject.balance).to eq(0) }
+
+  it 'adds a tenner to balance' do
+    expect { subject.top_up(20) }.to change { subject.balance }.from(0).to 20
+  end
   
 end
