29295959c1c14ee56387ab19776740fd98943a02
AJ <aj.monty@gmail.com>
2020-11-30 17:13:17 +0000

    added test and code for deduct method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,14 +6,21 @@ describe Oystercard do
 
   it { expect(subject.balance).to eq(0) }
 
-  it 'adds a tenner to balance' do
-    expect { subject.top_up(20) }.to change { subject.balance }.from(0).to 20
-  end
+  describe '#top_up' do
+    it 'adds 20 to balance' do
+      expect { subject.top_up(20) }.to change { subject.balance }.from(0).to 20
+    end
   
-  describe "#limit" do
     it "throws error if try to top up balance over limit" do
       subject.top_up(Oystercard::BALANCE_LIMIT)
       expect{ subject.top_up(10) }.to raise_error(RuntimeError, "Balance limit of #{Oystercard::BALANCE_LIMIT} exceeded")
+    end
+  end
+  
+  describe '#decuct' do
+    it 'takes 20 of the balance' do
+        subject.top_up(50)
+        expect { subject.deduct(20) }.to change { subject.balance }.from(50).to 30
     end
   end
 end
