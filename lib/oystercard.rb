f5b65be5876f63e446f88868cf4383f8daeac8fb
Luke Wickens <lukewickens1989@gmail.com>
2020-11-30 17:03:45 +0000

    Added constant balance limit and tested exceeding limit throws error

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,23 @@ class Oystercard
   
   attr_reader :balance
 
-  def initialize
+  BALANCE_LIMIT = 90
+
+  def initialize(balance_limit = BALANCE_LIMIT)
     @balance = 0
+    @balance_limit = balance_limit
   end
 
   def top_up(amount)
+    limit(amount)
     @balance += amount
   end
+
+  private
+  
+  def limit(amount)
+    limit = @balance + amount
+    fail "Balance limit of #{@balance_limit} exceeded" if limit > @balance_limit
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,4 +10,10 @@ describe Oystercard do
     expect { subject.top_up(20) }.to change { subject.balance }.from(0).to 20
   end
   
+  describe "#limit" do
+    it "throws error if try to top up balance over limit" do
+      subject.top_up(Oystercard::BALANCE_LIMIT)
+      expect{ subject.top_up(10) }.to raise_error(RuntimeError, "Balance limit of #{Oystercard::BALANCE_LIMIT} exceeded")
+    end
+  end
 end
